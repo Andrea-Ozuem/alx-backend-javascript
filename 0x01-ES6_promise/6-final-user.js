50df0bb28fcbfc2f020aca72085a6ec4c47237d4
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-09-27 20:04:55 +0100

    test another format

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+// Define the handleProfileSignup function
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values.map((val) => ({ status: val.status, value: val.status === 'fulfilled' ? val.value : val.reason })));
+  // Create Promises for signUpUser and uploadPhoto
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  // Use Promise.allSettled to wait for all promises to settle
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
