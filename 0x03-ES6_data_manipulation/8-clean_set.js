c8e795fc77eb96b78685e30fd3885a9b62658ea3
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-03 15:17:47 +0100

    fix: handle case

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, startString) {
+  if (!startString || !startString.length) return '';
   const res = [];
   for (const item of set) {
     if (item.startsWith(startString) && startString) {
