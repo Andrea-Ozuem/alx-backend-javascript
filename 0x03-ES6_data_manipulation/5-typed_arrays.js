daef858e3f926c44884e4231bf8f2790f82ada62
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-03 14:41:35 +0100

    fix: Throw err

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   const dv = new DataView(new ArrayBuffer(length));
-  try {
-    dv.setInt8(position, value);
-  } catch (e) {
-    console.error('Position outside range');
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
   }
+  dv.setInt8(position, value);
   return dv;
 }
